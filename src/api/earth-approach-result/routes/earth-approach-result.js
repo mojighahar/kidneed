910c651fa2085c87bd9e65ec9c3a9c813c1f343b
morshedi <mohammad.morshedi.73@gmail.com>
2022-03-10 15:44:08 +0330

    add: models models: earth-approach-result, earth_steps_results

---
+++ src/api/earth-approach-result/routes/earth-approach-result.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * earth-approach-result router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::earth-approach-result.earth-approach-result');
