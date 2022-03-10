e19c93d556833d4a65aa16224a78da2b2905d07b
alireza <areza9494@gmail.com>
2022-03-10 18:59:03 +0330

    fix

---
+++ src/api/step-psychologie/routes/step-psychologie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * step-psychologie router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::step-psychologie.step-psychologie');
